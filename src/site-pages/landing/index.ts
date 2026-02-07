a53cdae2fc66d0a898092bd2d26ac31aa452ea1e
Abel <malebochester@gmail.com>
2026-02-07 22:27:41 +0200

    update: remove wifi-fade.svg from landing page

--- src/site-pages/landing/index.ts
+++ src/site-pages/landing/index.ts
@@ -1,6 +1,5 @@
 import Navbar from '../landing/.l@nding/navbar';
 import Hero from '../landing/.l@nding/hero';
 import Footer from '../landing/.l@nding/footer';
-import '../landing/.lo@ding/wifi-fade.svg';
 
 export { Navbar, Hero, Footer };
